1d70bde72c8e86380483e814ea9a699d90f98b88
gani825 <kgee0503@gmail.com>
2025-05-05 07:08:55 +0900

    FitnessCalendar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // BrowserRouter 임포트
+import { HashRouter } from 'react-router-dom' // BrowserRouter 임포트
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 
 );
